4a5bea3a2a4dbcd98451bccc195ba01041d80723
bimaaidil <bimaaidil208@gmail.com>
2024-04-06 22:54:33 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,102 @@
+<template>
+  
+  <header class="header">
+      <a href="#" class="logo">Portofolio</a>
+
+      <nav class="navbar">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#experince">Skil</a>
+        <a href="#Project">Project</a>
+      </nav>
+    </header>
+
+    <!-- Bagian Homepage -->
+    <section class="home" id="home">
+      <div class="home-sosmed">
+        <a href="https://www.instagram.com/_bimaaidil?igsh=ajZmOWhpeWFiMjI4"><i class='bx bxl-instagram'></i></a>
+        <a href="https://github.com/bimaaidil"><i class='bx bxl-github'></i></a>
+        <a href="https://www.tiktok.com/@bima642?_t=8lIrrINWFei&_r=1"><i class='bx bxl-tiktok'></i></a>
+      </div>
+
+      <div class="content-home">
+        <h1><span style="color: #28b0e2; font-size: 58px;
+          font-weight: 700;">I'm</span> Bima Aidil</h1>
+        <h3>Mahasiswa Teknik Informatika</h3>
+        <br />
+        <p>
+          Student informatics engineering who are excited and always curious about the latest new technology. 
+        Having an interest in becoming an Game Developer with strong analytical skills and programming logic and can work in individually or group.
+        </p>
+        <a href="mailto:nurkurnia106@gmail.com" class="btn">Dowload CV</a>
+      </div>
+      <div class="image-banner"><img src="./assets/img/1.jpg" alt="Bima Aidil" width="357px" height="448"></div>
+    </section>
+
+
+     <!-- Bagian About -->
+    <section class="about" id="about">
+      <div class="image-banner">
+        <img src="./assets/img/2.jpg" />
+      </div>
+      <div class="content-about">
+        <h1></h1>
+        <h3>Teknik Informatika Universitas Islam Riau</h3>
+        <h3>Article</h3>
+        <ul>
+          <li>Rancang Bangun Aplikasi Cerdas Berbasis Android Untuk Deteksi Penyakit Tanaman Kopi Menggunakan Algoritma Deep Learning.</li>
+          <li>Developing an Android-based Smart Application for Detecting Coffee Plant Leaves Disease Using Deep Learning Algorithm.</li>
+          <li>Pengembanagn Aplikasi Mobile HR System dengan Framework Flutter di PT Baracipta Esa Enginering.</li>
+        </ul>
+        <p></p>
+      </div>
+    </section>
+
+
+     <!-- Bagian Eperience -->
+    <section class="experince" id="experince">
+      <h1 class="heading" style="text-align: center; font-size: 56px; ">Skil</h1>
+      <div class="container-exp">
+       <div class="box-exp">
+        <i class='bx bx-code-alt' style="font-size: 56px; background: transparent;"></i>
+        <h3>Mobile Apps Developer</h3>
+        
+        <br>
+        
+       </div>
+       <div class="box-exp">
+        <i class='bx bxs-briefcase' style="font-size: 56px; background: transparent;" ></i>
+        <h3>Android Learning Path</h3>
+        
+        <br>
+        
+       </div>
+       <div class="box-exp">
+        <i class='bx bx-edit' style="font-size: 56px; background: transparent;"></i>
+        <h3>UI/UX Desain Mobile</h3>
+        
+        <br>
+        
+       </div>
+      </div>  
+    </section>
+
+    <section class="Project" id="Project" style="background-color: rgb(255, 255, 255);">
+      <div id="portfolio">
+        <div class="container">
+          <h1 class="heading" style="text-align: center; font-size: 56px; ">Project</h1>
+            <div class="work-list">
+                <div class="work">
+                    <img src="">
+                    <div class="layer">
+                        <h3>COMING SOON</h3>
+                        <p>I'm still working on some project, will be update soon</p>
+                            <a href="#"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+                    </div>
+                </div>
+            </div>
+            <a href="#" class="btn">See more</a>
+        </div>
+    </div>
+    </section>
+</template>
